cad0e9b98011bec575deee06eb015b8bf2a03fbf
Adnan Wahab <mail@adnanwahab.com>
2014-06-21 12:19:12 -0700

    exec

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,14 @@
 javascript:(function () {
-  inject('http://d3js.org/d3.v3.min.js')
+  inject('http://d3js.org/d3.v3.min.js');
 
   if (window.context)
-    inject('adnan-wahab.github.io/Render-Tree-Visualization/vis.js')
+    inject('https://adnan-wahab.github.io/Render-Tree-Visualization/vis.js');
   else
-    console.log('visualization requires context to be exposed globally as window.context')
+    console.log('visualization requires context to be exposed globally as window.context');
 
   function inject(src) {
-    var s = document.createElement('script')
-    s.src = src
-    document.head.appendChild(s)
+    var s = document.createElement('script');
+    s.src = src;
+    document.head.appendChild(s);
   }
 })
